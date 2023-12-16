bd4531d97c3268333fa75601d3a45f963c9c72f5
Philipp Meinen <philipp@bind.ch>
2023-12-16 22:59:12 +0100

    simply naming

--- aoc23/day16/day16.go
+++ aoc23/day16/day16.go
@@ -24,7 +24,7 @@ func ProcessPart1(name string) {
 	lines := lib.ReadLines(name)
 	grid := ParseGrid(lines)
 	t := time.Now()
-	grid.FollowAndMark(0, 0, LeftToRight)
+	grid.FollowAndMark(0, 0, Right)
 	fmt.Println("Marking done", time.Now().Sub(t))
 	fmt.Println("Analysing energized fields")
 	grid.PrintEnergized()
@@ -49,25 +49,25 @@ func Part2BruteForce(grid *Grid) int {
 	var e int
 	// top row down
 	for col := 0; col < cols; col++ {
-		grid.FollowAndMark(0, col, TopToBottom)
+		grid.FollowAndMark(0, col, Down)
 		e = max(e, grid.CountEnergized())
 		grid.ResetEnergized()
 	}
 	// bottom row up
 	for col := 0; col < cols; col++ {
-		grid.FollowAndMark(rows-1, col, BottomToTop)
+		grid.FollowAndMark(rows-1, col, Up)
 		e = max(e, grid.CountEnergized())
 		grid.ResetEnergized()
 	}
 	// left column to the right
 	for row := 0; row < rows; row++ {
-		grid.FollowAndMark(row, 0, LeftToRight)
+		grid.FollowAndMark(row, 0, Right)
 		e = max(e, grid.CountEnergized())
 		grid.ResetEnergized()
 	}
 	// right column to the left
 	for row := 0; row < rows; row++ {
-		grid.FollowAndMark(row, cols-1, RightToLeft)
+		grid.FollowAndMark(row, cols-1, Left)
 		e = max(e, grid.CountEnergized())
 		grid.ResetEnergized()
 	}
@@ -84,14 +84,14 @@ func log(v int, msg string) {
 
 type Grid struct {
 	lib.Grid[byte]
-	LaserDirections lib.Grid[Directions]
+	LaserDirection lib.Grid[Direction]
 }
 
 const (
-	TopToBottom Directions = 1 << 0
-	BottomToTop Directions = 1 << 1
-	LeftToRight Directions = 1 << 2
-	RightToLeft Directions = 1 << 3
+	Down  Direction = 1 << 0
+	Up    Direction = 1 << 1
+	Right Direction = 1 << 2
+	Left  Direction = 1 << 3
 )
 
 const (
@@ -102,22 +102,22 @@ const (
 	SplitterHorizontal byte = '-'
 )
 
-type Directions int
+type Direction int
 
-func (d Directions) Set(dir Directions) Directions { return d | dir }
-func (d Directions) IsSet(dir Directions) bool     { return d&dir == dir }
-func (d Directions) IsHorizontal() bool            { return d == LeftToRight || d == RightToLeft }
-func (d Directions) IsVertical() bool              { return d == TopToBottom || d == BottomToTop }
-func (d Directions) String() string {
+func (d Direction) Add(dir Direction) Direction { return d | dir }
+func (d Direction) Contains(dir Direction) bool { return d&dir == dir }
+func (d Direction) IsHorizontal() bool          { return d == Right || d == Left }
+func (d Direction) IsVertical() bool            { return d == Down || d == Up }
+func (d Direction) String() string {
 	switch d {
-	case TopToBottom:
-		return "TopToBottom"
-	case BottomToTop:
-		return "BottomToTop"
-	case LeftToRight:
-		return "LeftToRight"
-	case RightToLeft:
-		return "RightToLeft"
+	case Down:
+		return "Down"
+	case Up:
+		return "Up"
+	case Right:
+		return "Right"
+	case Left:
+		return "Left"
 	default:
 		return "Unknown"
 	}
@@ -126,25 +126,25 @@ func (d Directions) String() string {
 func ParseGrid(lines []string) *Grid {
 	grid := lib.NewByteGridFromStrings(lines)
 	return &Grid{
-		Grid:            lib.NewByteGridFromStrings(lines),
-		LaserDirections: lib.NewGrid[Directions](grid.Rows(), grid.Columns()),
+		Grid:           lib.NewByteGridFromStrings(lines),
+		LaserDirection: lib.NewGrid[Direction](grid.Rows(), grid.Columns()),
 	}
 }
 
-func (g *Grid) FollowAndMark(row, col int, dir Directions) {
+func (g *Grid) FollowAndMark(row, col int, dir Direction) {
 	if !g.IsValidPosition(row, col) {
 		// laser leaving the grid is ignored
 		return
 	}
-	v := g.LaserDirections.Get(row, col)
-	if v.IsSet(dir) {
+	ld := g.LaserDirection.Get(row, col)
+	if ld.Contains(dir) {
 		if VERBOSE >= 2 {
-			fmt.Printf("(%d,%d) already visited %s\n", row, col, v)
+			fmt.Printf("(%d,%d) already visited %s\n", row, col, ld)
 		}
 		return // already marked this direction
 	}
-	v = v.Set(dir)
-	g.LaserDirections.Set(row, col, v)
+	ld = ld.Add(dir)
+	g.LaserDirection.Set(row, col, ld)
 	if VERBOSE >= 2 {
 		fmt.Printf("(%d,%d) following %s\n", row, col, dir)
 	}
@@ -160,10 +160,10 @@ func (g *Grid) FollowAndMark(row, col int, dir Directions) {
 			g.FollowAndMark(row, col, dir)
 		} else {
 			// beam is split
-			rowA, colA := NextPosition(row, col, BottomToTop)
-			g.FollowAndMark(rowA, colA, BottomToTop)
-			rowB, colB := NextPosition(row, col, TopToBottom)
-			g.FollowAndMark(rowB, colB, TopToBottom)
+			rowA, colA := NextPosition(row, col, Up)
+			g.FollowAndMark(rowA, colA, Up)
+			rowB, colB := NextPosition(row, col, Down)
+			g.FollowAndMark(rowB, colB, Down)
 		}
 	case SplitterHorizontal: // -
 		if dir.IsHorizontal() {
@@ -172,19 +172,19 @@ func (g *Grid) FollowAndMark(row, col int, dir Directions) {
 			g.FollowAndMark(row, col, dir)
 		} else {
 			// beam is split
-			rowA, colA := NextPosition(row, col, LeftToRight)
-			g.FollowAndMark(rowA, colA, LeftToRight)
-			rowB, colB := NextPosition(row, col, RightToLeft)
-			g.FollowAndMark(rowB, colB, RightToLeft)
+			rowA, colA := NextPosition(row, col, Right)
+			g.FollowAndMark(rowA, colA, Right)
+			rowB, colB := NextPosition(row, col, Left)
+			g.FollowAndMark(rowB, colB, Left)
 		}
 
 	case MirrorSlash: // /
-		dir = TranslateSlash(dir)
+		dir = RedirectSlash(dir)
 		row, col = NextPosition(row, col, dir)
 		g.FollowAndMark(row, col, dir)
 
 	case MirrorBackSlash: // \
-		dir = TranslateBashSlash(dir)
+		dir = RedirectBashSlash(dir)
 		row, col = NextPosition(row, col, dir)
 		g.FollowAndMark(row, col, dir)
 
@@ -193,46 +193,47 @@ func (g *Grid) FollowAndMark(row, col int, dir Directions) {
 	}
 }
 
-func TranslateSlash(dir Directions) Directions {
+func RedirectSlash(dir Direction) Direction {
 	// /
 	switch dir {
-	case BottomToTop:
-		return LeftToRight
-	case TopToBottom:
-		return RightToLeft
-	case LeftToRight:
-		return BottomToTop
-	case RightToLeft:
-		return TopToBottom
-	default:
-		panic("invalid direction")
-	}
-}
-func TranslateBashSlash(dir Directions) Directions {
-	// \
-	switch dir {
-	case BottomToTop:
-		return RightToLeft
-	case TopToBottom:
-		return LeftToRight
-	case LeftToRight:
-		return TopToBottom
-	case RightToLeft:
-		return BottomToTop
+	case Up:
+		return Right
+	case Down:
+		return Left
+	case Right:
+		return Up
+	case Left:
+		return Down
 	default:
 		panic("invalid direction")
 	}
 }
 
-func NextPosition(row, col int, dir Directions) (int, int) {
+func RedirectBashSlash(dir Direction) Direction {
+	// \
 	switch dir {
-	case BottomToTop:
+	case Up:
+		return Left
+	case Down:
+		return Right
+	case Right:
+		return Down
+	case Left:
+		return Up
+	default:
+		panic("invalid direction")
+	}
+}
+
+func NextPosition(row, col int, dir Direction) (int, int) {
+	switch dir {
+	case Up:
 		return row - 1, col
-	case TopToBottom:
+	case Down:
 		return row + 1, col
-	case LeftToRight:
+	case Right:
 		return row, col + 1
-	case RightToLeft:
+	case Left:
 		return row, col - 1
 	default:
 		panic("invalid input")
@@ -244,7 +245,7 @@ func (g *Grid) CountEnergized() int {
 	energized := 0
 	for r := 0; r < rows; r++ {
 		for c := 0; c < cols; c++ {
-			if g.LaserDirections.Get(r, c) != 0 {
+			if g.LaserDirection.Get(r, c) != 0 {
 				energized++
 			}
 		}
@@ -256,7 +257,7 @@ func (g *Grid) ResetEnergized() {
 	rows, cols := g.Rows(), g.Columns()
 	for r := 0; r < rows; r++ {
 		for c := 0; c < cols; c++ {
-			g.LaserDirections.Set(r, c, 0)
+			g.LaserDirection.Set(r, c, 0)
 		}
 	}
 }
@@ -265,7 +266,7 @@ func (g *Grid) PrintEnergized() {
 	rows, cols := g.Rows(), g.Columns()
 	for r := 0; r < rows; r++ {
 		for c := 0; c < cols; c++ {
-			if g.LaserDirections.Get(r, c) != 0 {
+			if g.LaserDirection.Get(r, c) != 0 {
 				fmt.Print("#")
 			} else {
 				fmt.Print(".")
