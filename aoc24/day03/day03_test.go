a08893317a387d326b5eb7004f501ed6891a1a32
Philipp Meinen <philipp@bind.ch>
2024-12-03 22:34:01 +0100

    aoc 2024 day 3: tests

--- aoc24/day03/day03_test.go
+++ aoc24/day03/day03_test.go
@@ -6,28 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseMul(t *testing.T) {
+func TestTestmul(t *testing.T) {
 	var tests = []struct {
 		input string
 		x, y  int
-		rem   string
+		next  int
 		ok    bool
 	}{
-		{"mul(4)", 0, 0, "", false},
-		{"mul(4,)", 0, 0, "", false},
-		{"mul(,444)", 0, 0, "", false},
-		{"mul(1,2)", 1, 2, "", true},
-		{"mul(1,2)mul", 1, 2, "mul", true},
-		{"mul(1,2]asdf)", 0, 0, "", false},
+		{"mul(4)", 0, 0, 0, false},
+		{"mul(4,)", 0, 0, 0, false},
+		{"mul(,444)", 0, 0, 0, false},
+		{"mul(1,2)", 1, 2, 8, true},
+		{"mul(1,22)", 1, 22, 9, true},
+		{"mul(1,2)mul", 1, 2, 8, true},
+		{"mul(1,2]asdf)", 0, 0, 0, false},
 	}
 
 	for _, test := range tests {
 		test := test
 		t.Run(test.input, func(t *testing.T) {
-			x, y, rem, ok := parseMul(test.input)
+			x, y, next, ok := testmul(test.input)
 			assert.Equal(t, test.x, x)
 			assert.Equal(t, test.y, y)
-			assert.Equal(t, test.rem, rem)
+			assert.Equal(t, test.next, next)
 			assert.Equal(t, test.ok, ok)
 		})
 	}
