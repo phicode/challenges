6c5a20507415c9fcc8be922e7e996b5c116bd3f2
Philipp Meinen <philipp@bind.ch>
2024-12-04 23:13:07 +0100

    aoc 2024 day 4 part 2

--- aoc24/day04/day04.go
+++ aoc24/day04/day04.go
@@ -15,9 +15,9 @@ var VERBOSE = 1
 func main() {
 	ProcessPart1("aoc24/day04/example.txt")
 	ProcessPart1("aoc24/day04/input.txt")
-	//
-	//ProcessPart2("aoc24/day04/example.txt")
-	//ProcessPart2("aoc24/day04/input.txt")
+
+	ProcessPart2("aoc24/day04/example.txt")
+	ProcessPart2("aoc24/day04/input.txt")
 }
 
 func ProcessPart1(name string) {
@@ -31,9 +31,9 @@ func ProcessPart1(name string) {
 func ProcessPart2(name string) {
 	fmt.Println("Part 2 input:", name)
 	lines := lib.ReadLines(name)
-	_ = lines
-
-	fmt.Println()
+	grid := ParseInput(lines)
+	count := grid.CountXmasP2()
+	fmt.Println("Count:", count)
 }
 
 func log(v int, msg string) {
@@ -67,6 +67,19 @@ func (g *Grid) CountXmas() int {
 	return total
 }
 
+func (g *Grid) CountXmasP2() int {
+	rows, cols := g.Size()
+	total := 0
+	for c := 1; c < cols-1; c++ {
+		for r := 1; r < rows-1; r++ {
+			if g.IsMasCenter(rowcol.Pos{Row: r, Col: c}) {
+				total++
+			}
+		}
+	}
+	return total
+}
+
 var xmas = [4]byte{'X', 'M', 'A', 'S'}
 var Directions = []rowcol.Direction{
 	rowcol.Left, rowcol.Right, rowcol.Up, rowcol.Down,
@@ -86,4 +99,39 @@ func (g *Grid) IsXmas(p rowcol.Pos, d rowcol.Direction) bool {
 func (g *Grid) IsValue(value byte, p rowcol.Pos) bool {
 	return g.IsValidPosition(p.Row, p.Col) &&
 		g.Get(p.Row, p.Col) == value
+}
+
+func (g *Grid) IsXmasP2(p rowcol.Pos, d rowcol.Direction) bool {
+	for i, val := range xmas {
+		if !g.IsValue(val, p.Add(d.MulS(i))) {
+			return false
+		}
+	}
+	return true
+}
+
+func (g *Grid) IsMasCenter(p rowcol.Pos) bool {
+	// Coordinates
+	// a.b
+	// .A.
+	// c.d
+	a := p.Add(rowcol.Pos(rowcol.UpLeft))
+	b := p.Add(rowcol.Pos(rowcol.UpRight))
+	c := p.Add(rowcol.Pos(rowcol.DownLeft))
+	d := p.Add(rowcol.Pos(rowcol.DownRight))
+	if !g.IsValidPosition(a.Row, a.Col) || !g.IsValidPosition(d.Row, d.Col) {
+		return false
+	}
+	if !g.IsValue('A', p) {
+		return false
+	}
+	return isMS(g.Get(a.Row, a.Col), g.Get(d.Row, d.Col)) &&
+		isMS(g.Get(b.Row, b.Col), g.Get(c.Row, c.Col))
+}
+
+func isMS(a, b byte) bool {
+	if a == 'M' {
+		return b == 'S'
+	}
+	return a == 'S' && b == 'M'
 }
--- aoc24/day04/notes.md
+++ aoc24/day04/notes.md
@@ -5,6 +5,25 @@ Part 1 input: aoc24/day04/example.txt
 Count: 18
 Part 1 input: aoc24/day04/input.txt
 Count: 2718
+Part 2 input: aoc24/day04/example.txt
+Count: 9
+Part 2 input: aoc24/day04/input.txt
+Count: 2046
 ```
 
 # Notes
+
+```text
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+```
+
+Y, X = 1, 2
