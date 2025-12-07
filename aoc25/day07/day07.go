063f7112da74eae561a94795bddcc638f182fa53
Philipp Meinen <philipp@bind.ch>
2025-12-07 10:20:20 +0100

    aoc 2025 day 7 part 1

---
+++ aoc25/day07/day07.go
@@ -0,0 +1,104 @@
+package main
+
+// https://adventofcode.com/2025/day/7
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/phicode/challenges/lib"
+	"github.com/phicode/challenges/lib/rowcol"
+)
+
+func main() {
+	flag.Parse()
+	lib.Timed("Part 1", ProcessPart1, "aoc25/day07/example.txt")
+	lib.Timed("Part 1", ProcessPart1, "aoc25/day07/input.txt")
+
+	lib.Timed("Part 2", ProcessPart2, "aoc25/day07/example.txt")
+	lib.Timed("Part 2", ProcessPart2, "aoc25/day07/input.txt")
+
+	//lib.Profile(1, "day07.pprof", "Part 2", ProcessPart2, "aoc25/day07/input.txt")
+}
+
+func ProcessPart1(name string) {
+	fmt.Println("Part 1 input:", name)
+	input := ReadAndParseInput(name)
+	result := SolvePart1(input)
+	fmt.Println("Result:", result)
+}
+
+func ProcessPart2(name string) {
+	fmt.Println("Part 2 input:", name)
+	input := ReadAndParseInput(name)
+	result := SolvePart2(input)
+	fmt.Println("Result:", result)
+}
+
+////////////////////////////////////////////////////////////
+
+const (
+	START    = 'S'
+	SPLITTER = '^'
+	FREE     = '.'
+	BEAM     = '|'
+)
+
+type Input struct {
+	grid rowcol.Grid[byte]
+}
+
+func ReadAndParseInput(name string) Input {
+	lines := lib.ReadLines(name)
+	return ParseInput(lines)
+}
+
+func ParseInput(lines []string) Input {
+	return Input{
+		grid: rowcol.NewByteGridFromStrings(lines),
+	}
+}
+
+////////////////////////////////////////////////////////////
+
+func SolvePart1(input Input) int {
+	grid := input.grid
+	current := grid.MustFindFirst(func(x byte) bool { return x == START })
+	splits := follow(grid, current)
+	return splits
+}
+
+func follow(g rowcol.Grid[byte], current rowcol.Pos) int {
+	if !g.IsValidPos(current) {
+		return 0
+	}
+	splits := 0
+	for {
+		next := current.AddDir(rowcol.Down)
+		if !g.IsValidPos(next) {
+			return splits
+		}
+		cell := g.GetPos(next)
+		switch cell {
+		case BEAM:
+			return splits // already accounted for
+		case FREE:
+			current = next
+			g.SetPos(next, BEAM)
+			continue
+		case SPLITTER:
+			splits++
+			left := next.AddDir(rowcol.Left)
+			right := next.AddDir(rowcol.Right)
+			return splits + follow(g, left) + follow(g, right)
+		default:
+			panic(fmt.Errorf("invalid cell value: %v", cell))
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////
+
+func SolvePart2(input Input) int {
+	return 0
+}
